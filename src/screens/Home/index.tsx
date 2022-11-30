090c5ae75f65d05d19b352dd38a0310bdeff29be
Weverton dos Reis <wevertondosreis@gmail.com>
2022-11-30 14:42:32 -0300

    Criação da informação da lista de tarefas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,56 @@
-import { View} from "react-native"
+import { useState } from "react"
+import { View, Text, FlatList } from "react-native"
 import { Header } from "../../components/Header"
 
 import { styles } from "./styles"
 
+type TaskItem={
+    id:string,
+    text:string,
+    completes:boolean
+}
+
 
 
 export function Home() {
+    const [tasks, setTasks] = useState<TaskItem[]>([])
     return (
         <View style={styles.container}>
-            <Header/>
+            <Header />
+            <View style={styles.tasks}>
+                <View style={styles.info}>
+
+                    <View style={styles.infoIten}>
+                        <Text style={[styles.textInfo, { color: '#4EA8DE' }]}>Criadas</Text>
+                        <Text style={styles.qtdInfo}>0</Text>
+                    </View>
+
+                    <View style={styles.infoIten}>
+                        <Text style={[styles.textInfo, { color: '#8284FA' }]}>Concluídas</Text>
+                        <Text style={styles.qtdInfo}>2</Text>
+                    </View>
+                </View>
+
+                <FlatList
+                    data={tasks}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Text>{item.text}</Text>
+
+
+                    )}
+
+                    showsVerticalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text >
+                            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença!
+                        </Text>
+                    )}
+                />
+
+
+
+            </View>
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,5 +4,39 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#1A1A1A',
+    },
+
+    tasks:{
+        marginVertical: 48,
+        marginHorizontal: 24,
+    },
+
+    info:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+       
+    },
+
+    infoIten:{
+        flexDirection: 'row',
+        alignItems:'center'
+       
+    },
+
+    textInfo:{
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginRight:8
+    },
+
+    qtdInfo:{
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: '#D9D9D9',
+        backgroundColor: '#333333',
+        width: 25,
+        textAlign: 'center',
+        borderRadius: 100
     }
 })
