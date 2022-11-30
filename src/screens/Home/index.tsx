8c5a28f707bd928e46f38ee672c886fd1822ec54
Weverton dos Reis <wevertondosreis@gmail.com>
2022-11-30 10:52:34 -0300

    Início do Projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+import { styles } from "./styles"
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Todo List</Text>
+        </View>
+    )
+}
